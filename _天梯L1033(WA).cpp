58051095e4d8eb6d8959053827a0c9ffb34dea7b
Nightwatch-Fox11 <399237518@qq.com>
2020-02-13 23:16:03 +0800

    .

---
+++ _天梯L1033(WA).cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int weishu(int n)
+{
+    int i;
+    for (i = 0; n != 0; i++)
+    {
+        n /= 10;
+    }
+    return i;
+}
+
+int main()
+{
+    int y, n;
+    cin >> y >> n;
+    switch (n)
+    {
+    case 2:
+        for (int i = 0; i < ; i++)
+        {
+            /* code */
+        }
+
+        break;
+    case 3:
+
+        break;
+
+    default:
+        break;
+    }
+
+    return 0;
+}
