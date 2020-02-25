26ca360e920dfbb695833e5137a43b02d7806aa5
Nightwatch-Fox11 <399237518@qq.com>
2020-02-25 22:17:41 +0800

    .

--- Method/vector.cpp
+++ Method/vector.cpp
@@ -4,7 +4,18 @@ using namespace std;
 
 int main()
 {
-    vector<int> vi; //defination
+    vector<int> vi;           //defination of vector
+    vector<int>::iterator it; //defination of iterator
+    for (int i = 0; i < 4; i++)
+    {
+        vi.push_back(i + 2); //similar to python's 'append'
+    }
+    //use of iterator
+    it = vi.begin();
+    for (int i = 0; i < 4; i++)
+    {
+        cout << *(it + i) << ' '; //v[i]==*(vi.begin()+i)
+    }
 
     return 0;
 }
