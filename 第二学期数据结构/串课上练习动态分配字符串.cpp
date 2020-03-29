ea4a62569498fac0ae25042fbf82534464f53e71
Nightwatch-Fox11 <399237518@qq.com>
2020-03-29 13:12:36 +0800

    .

---
+++ 第二学期数据结构/串课上练习动态分配字符串.cpp
@@ -0,0 +1,28 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+#define MAXLEN 100
+typedef struct
+{
+    char ch[MAXLEN];
+    int len;
+} SeqString;
+
+int StrInsert(SeqString *s, int pos, SeqString t, char *p)
+{
+    p = (char *)malloc(t.len + s->len);
+    for (int i = 0; i < pos; i++)
+    {
+        p[i] = s->ch[i];
+    }
+    for (int i = 0; i < strlen(p); i++)
+    {
+        p[pos + i] = t.ch[i];
+    }
+    int a = strlen(p);
+    for (int i = pos; i < s->len; i++)
+    {
+        p[a + i] = s->ch[i];
+    }
+}
