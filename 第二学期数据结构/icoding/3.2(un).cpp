0c9149e840bb95901ba4dd7a858aaf8d408485ce
Nightwatch-Fox11 <399237518@qq.com>
2020-04-07 20:16:41 +0800

    .

--- 第二学期数据结构/icoding/3.2(un).cpp
+++ 第二学期数据结构/icoding/3.2(un).cpp
@@ -1,82 +1,100 @@
 #include <stdio.h>
-#include <stdlib.h>
-
+int length(char *a);
+int _find(char *a, char *b, int pos);
+void Delete(char *a, int pos, int len);
+void insert(char *a, int pos, char *b);
 int str_replace(const char *in, char *out, int outlen, const char *oldstr, const char *newstr)
 {
-    int lenin = 0, lenold = 0, lennew = 0, t = 0;
+    int lenin = length(in), lenold = length(oldstr), lennew = length(newstr), m = _find(in, oldstr, 0), t = 0; //t为匹配次数
     // 统计长度
-    while (in[lenin] != '\0')
+    char outt[outlen];
+    for (int k = 0; k < lenin; k++)
     {
-        lenin++;
+        outt[k] = in[k];
     }
-    while (oldstr[lenold] != '\0')
+    while (m && length(outt) - lenold + lennew < outlen)
     {
-        lenold++;
-    }
-    while (newstr[lennew] != '\0')
-    {
-        lennew++;
-    }
-    int lenout = lenin, i = 0;
-    // BF找字串
-
-    while (1)
-    {
-        int j = 0;
-        while (i < lenin && j < lenold)
+        Delete(outt, m, lenold);
+        insert(outt, m, newstr);
+        m += lennew;
+        m = _find(outt, oldstr, m);
+        t++;
+        for (int k = 0; k < length(outt); k++)
         {
-            if (in[i] == oldstr[j])
-            {
-                i++;
-                j++;
-            }
-            else
-            {
-                i = i - j + 1;
-                j = 0;
-            }
-        }
-        if (j >= lenold)
-        {
-
-            if (lenout + lennew >= outlen)
-            {
-                out[lenout + 1] = '\0';
-                return t;
-            }
-            else
-            {
-                if (t == 0)
-                {
-                    for (int k = 0; k < i - lenold; k++)
-                    {
-                        out[k] = in[k];
-                    }
-                    lenout = i - lenold;
-                }
-                for (int k = 0; k < lennew; k++)
-                {
-                    out[lenout + k] = newstr[k];
-                }
-                lenout += lennew;
-                for (int k = 0; k + i < lenin; k++)
-                {
-                    if (lenout + k == outlen - 1)
-                    {
-                        out[outlen - 1] = '\0';
-                        break;
-                    }
-
-                    out[lenout + k] = in[i + k];
-                }
-
-                t++;
-            }
-        }
-        else
-        {
-            break;
+            out[k] = outt[k];
         }
     }
     return t;
+}
+void insert(char *a, int pos, char *b)
+{
+    int lena = length(a), lenb = length(b), i;
+    for (i = lena + lenb - 1; i >= lenb + pos; i--)
+    {
+        a[i] = a[i - lenb];
+    }
+    for (i = 0; i < lenb; i++)
+    {
+        a[i + pos] = b[i];
+    }
+    a[lena + lenb] = '\0';
+}
+void Delete(char *a, int pos, int len)
+{
+    int lena = length(a), i;
+    for (i = pos + len; i < lena; i++)
+    {
+        a[i - len] = a[i];
+    }
+    a[lena - len] = '\0';
+}
+int _find(char *a, char *b, int pos)
+{
+    int lena = length(a), lenb = length(b);
+    int i = pos;
+    int j = 0;
+    if (pos < 0)
+    {
+        return 0;
+    }
+    while (i < lena && j < lenb)
+    {
+        if (a[i] == b[j])
+        {
+            i++;
+            j++;
+        }
+        else
+        {
+            i = i - j + 1;
+            j = 0;
+        }
+    }
+    return j >= lenb ? i - lenb : 0;
+}
+int length(char *a)
+{
+    int temp = 0;
+    while (a[temp] != '\0')
+    {
+        temp++;
+    }
+    return temp;
+}
+int main()
+{
+    char l[120];
+    /*
+    char m[20] = "IUdaHUIH";
+    char u[20] = "aHU";
+    printf("%d\n", length(q));
+    printf("%d", _find(m, u, 0));
+    Delete(m, 1, 2);
+    insert(m, 1, u);
+    printf("%s", m);
+    */
+    // int m = str_replace("abcdefghijklmnopqrabcdefghijklmnopqrabcdefghijklmnopqrabcdefghijklmnopqr", l, 100, "pqrabcdef", "##icoding.run##");
+    int m = str_replace("aaabbbccc", l, 14, "c", "123");
+    printf("%d\n%s", m, l);
+    return 0;
 }
---
+++ 第二学期数据结构/icoding/3.3(un).cpp
@@ -0,0 +1,90 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dsstring.h" // 请不要删除，否则检查不通过
+#define BLOCK_SIZE 4  // 可由用户定义的块大小
+#define BLS_BLANK '#' // 用于空白处的补齐字符
+
+typedef struct _block
+{
+    char ch[BLOCK_SIZE]; //块的数据域
+    struct _block *next; //块的指针域
+} Block;
+
+typedef struct
+{
+    Block *head; // 串的头指针
+    Block *tail; // 串的尾指针
+    int len;     // 串的当前长度
+} BLString;
+
+bool blstr_substr(BLString src, int pos, int len, BLString *sub)
+{
+    int i, j, k, count;
+    Block *r, *p;
+    if (src.head == NULL && src.tail == NULL && src.len == 0)
+    {
+        return 0;
+    }
+    if (pos < 0 || pos >= src.len || len <= 0)
+    {
+        return 0;
+    }
+    for (count = 1, p = src.head; pos >= 4 * count; count++, p = p->next) //令p就位
+    {
+        ;
+    }
+    r = (*sub).head;
+    i = 0;
+    j = 0;
+    k = pos % 4;
+    while (i < len && (*sub).len < src.len)
+    {
+        if (!r)
+        {
+            r = (Block *)malloc(sizeof(Block));
+            if (!r)
+            {
+                return 0;
+            }
+            r->next = NULL;
+            if (!(*sub).head)
+            {
+                (*sub).head = (*sub).tail = r;
+            }
+            else
+            {
+                (*sub).tail->next = r;
+                (*sub).tail = r;
+            }
+        }
+        while (i < len && (*sub).len <= src.len)
+        {
+            (*r).ch[j] = (*p).ch[k];
+            j = (j + 1) % 4;
+            k = (k + 1) % 4;
+            (*sub).len++;
+            i++;
+            if (!k)
+            {
+                p = p->next;
+            }
+            if (!j)
+            {
+                r = r->next;
+                break;
+            }
+        }
+    }
+
+    if ((*sub).len == src.len)
+    {
+        (*sub).len--;
+    }
+    count = ((*sub).len - 1) % 4 + 1;
+    while (count < 4)
+    {
+        (*sub).tail->ch[count] = '#';
+        count++;
+    }
+    return 1;
+}
