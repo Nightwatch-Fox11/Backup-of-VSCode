d263d320eb98a4d3e85389e8fb02ed5fe3a92e60
Nightwatch-Fox11 <399237518@qq.com>
2020-05-05 14:17:10 +0800

    .

--- ACM集训/数据结构/A.cpp
+++ ACM集训/数据结构/A.cpp
@@ -1,5 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
+//法一 模拟栈过程 un
+/*
 int main()
 {
     stack<char> st;
@@ -25,6 +27,63 @@ int main()
             {
             }
         }
+    }
+
+    return 0;
+}
+*/
+
+/*
+法二： 不使用用栈，直接+1-1  AC
+思路是每一次设置一个计数器cnt为0
+读入 （ 就cnt++ ，读入 ） 就cnt--  
+当cnt==0时遇到）则answer++
+最后答案==answer+cnt（cnt相当于剩下的未匹配的括号）
+*/
+int main()
+{
+    int T, answer;
+    cin >> T;
+    if (!T)
+    {
+        cout << 0 << endl;
+        return 0;
+    }
+
+    while (T--)
+    {
+        answer = 0;
+        int n, cnt = 0;
+        cin >> n;
+
+        if (!n)
+        {
+            cin.get();
+            cout << 0 << endl;
+            continue;
+        }
+
+        string a;
+        cin >> a;
+        for (int i = 0; i < a.size(); i++)
+        {
+            if (a[i] == '(')
+            {
+                cnt++;
+            }
+            else
+            {
+                if (!cnt)
+                {
+                    answer++;
+                }
+                else
+                {
+                    cnt--;
+                }
+            }
+        }
+        cout << answer + cnt << endl;
     }
 
     return 0;
--- 第二学期数据结构/icoding/5.2(rev).cpp
+++ 第二学期数据结构/icoding/5.2(rev).cpp
@@ -21,7 +21,7 @@ bool push(Stack *S, ElemType x);  //x 入栈
 bool pop(Stack *S, ElemType *px); //出栈，元素保存到px所指的单元，函数返回true,栈为空时返回 false
 bool top(Stack *S, ElemType *px); //获取栈顶元素，将其保存到px所指的单元，函数返回true，栈满时返回 false
 bool is_empty(Stack *S);          // 栈为空时返回 true，否则返回 false
-
+//97(un)
 bool path(BiTNode *root, BiTNode *node, Stack *s)
 {
     init_stack(s);
@@ -53,6 +53,33 @@ bool path(BiTNode *root, BiTNode *node, Stack *s)
                 p = p->right;
             }
         }
+    }
+    return false;
+}
+//AC
+bool path(BiTNode *root, BiTNode *node, Stack *s)
+{
+    BiTree T = root, p;
+    if (T == NULL || node == NULL || !is_empty(s))
+        return false;
+    while (T || !is_empty(s))
+    {
+        while (T)
+        {
+            push(s, T);
+            if (T == node)
+                return true;
+            T = T->left;
+        }
+        top(s, &T);
+        if (!T->right || T->right == p)
+        {
+            p = T;
+            pop(s, &T);
+            T = NULL;
+        }
+        else
+            T = T->right;
     }
     return false;
 }
