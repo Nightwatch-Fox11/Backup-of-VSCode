ff7eefb9249095c4abd65537a795c9f7e4fd2f95
Nightwatch-Fox11 <399237518@qq.com>
2020-06-09 21:48:47 +0800

    .

--- Method/Bubble_Sort.c
+++ Method/Bubble_Sort.c
@@ -22,3 +22,23 @@ void bubbleSort(int arr[], int len)
             }
         }
 }
+//优化版本
+// 假设数组的大小是n+1，冒泡排序从数组下标1开始
+void bubble_sort(int *a, int n)
+{
+    bool flag = true;
+    while (flag)
+    {
+        flag = false;
+        for (int i = 1; i < n; ++i)
+        {
+            if (a[i] > a[i + 1])
+            {
+                flag = true;
+                int t = a[i];
+                a[i] = a[i + 1];
+                a[i + 1] = t;
+            }
+        }
+    }
+}
